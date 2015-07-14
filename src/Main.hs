2a1fb19e016a79649acd82498103391e144a1468
Richard Davison <richivinsky@gmail.com>
2015-07-13 20:49:40 -0400

    Refactored, cleaned up, and commented the code so it is easier to read

--- src/Main.hs
+++ src/Main.hs
@@ -12,71 +12,102 @@ import Control.Applicative
 import Control.Monad (zipWithM_)
 import Control.Monad.Loops (untilM)
 
-glossika :: FilePath
-glossika = "res/GLOSSIKA-ENJA-F1-GMS-0001A.wav"
+pauseSampleLength = 20500
+deadSilence = 128
+(silenceRangeStart, silenceRangeEnd) = (126, 130)
 
-getAudio :: FilePath -> IO (Audio Word8)
-getAudio path = do
+readWavFile :: FilePath -> IO (Audio Word8)
+readWavFile path = do
     maybeAudio <- importFile path
     case maybeAudio of
         Left s -> undefined
         Right t -> return t
     
 isSilence :: Word8 -> Bool
-isSilence x = and checkResults where
-    lowerBound = 128
-    upperBound = 128
-    checkResults = do
-        f <- [(lowerBound >=), (upperBound <=)]
-        return $ f x
+isSilence x = x == deadSilence
         
-noise1 :: A.Parser BS.ByteString
-noise1 = A.takeWhile $ not . isSilence
+parseSoundChunk :: A.Parser BS.ByteString
+parseSoundChunk = A.takeWhile $ not . isSilence
 
-mySilence :: A.Parser (Maybe BS.ByteString)
-mySilence = go 0 BS.empty where
+parseMaybeSilentChunk :: A.Parser (Maybe BS.ByteString)
+parseMaybeSilentChunk = go 0 BS.empty where
     go i acc = do
         x <- A.peekWord8
         case x of
-            Just x' -> if or [x' == 126, x' == 127, x' == 128, x' == 129, x' == 130]
-                           then do
-                               A.take 1
-                               go (i+1) $ x' `BS.cons` acc
-                           else if i < 20500
-                                    then return $ Just acc
-                                    else return $ Nothing
-            Nothing -> return $ Nothing
+            Nothing -> return Nothing
+            Just x' ->
+                if or $ fmap (x' ==) [silenceRangeStart..silenceRangeEnd]
+                    then do
+                        A.take 1 -- consume the byte
+                        go (i+1) $ x' `BS.cons` acc
+                    else if i < pauseSampleLength
+                        then return $ Just acc
+                        else return Nothing
 
-myChunk :: A.Parser BS.ByteString
-myChunk = go BS.empty where
+parseChunk :: A.Parser BS.ByteString
+parseChunk = go BS.empty where
     go acc = do
-        noisyChunk <- noise1
-        silentChunk <- mySilence
-        case silentChunk of
-            Just chunk -> go $ acc' where acc' = acc `BS.append` (noisyChunk `BS.append` chunk)
-            Nothing    -> return $ acc `BS.append` noisyChunk
+        soundChunk <- parseSoundChunk
+        maybeSilentChunk <- parseMaybeSilentChunk
+        case maybeSilentChunk of
+            Nothing           -> return $ acc `BS.append` soundChunk
+            Just silentChunk' -> go $ acc' where acc' = acc `BS.append` (soundChunk `BS.append` silentChunk')
 
-parseGlossika :: A.Parser [BS.ByteString]
-parseGlossika = myChunk `untilM` (A.atEnd)
+parseSplitOnPause :: A.Parser [BS.ByteString]
+parseSplitOnPause = parseChunk `untilM` (A.atEnd)
 
-getFilename :: Int -> FilePath
-getFilename n = "output/out-" ++ show n ++ ".wav"
+mkOutFilename :: Int -> FilePath
+mkOutFilename n = "output/out-" ++ show n ++ ".wav"
+
+saveResults :: [Audio Word8] -> IO ()
+saveResults = zipWithM_ exportFile outputFilenames
+    where
+        outputFilenames = fmap mkOutFilename [1..]
+
+toAudio :: Int -> Int -> BS.ByteString -> Audio Word8
+toAudio sampleRate' channelNumber' rawResults
+    = Audio
+    {   sampleRate    = sampleRate'
+    ,   channelNumber = channelNumber'
+    ,   sampleData    = toSampleData . toList $ rawResults }
+    where
+        toList = BS.unpack
+        toSampleData = \x -> array (0, (length x) - 1) (zip [0..] x)
 
 main :: IO ()
 main = do
-    -- Get the audio and print stats
-    rawAudio <- getAudio glossika
+    -- Specify the file we want to parse
+    let filename = "res/GLOSSIKA-ENJA-F1-GMS-0001A.wav"
+
+    -- Read the file
+    putStrLn $ "Reading in file " ++ filename
+    rawAudio <- readWavFile filename
+    putStrLn ""
+
+    -- Print information about the parsed file
+    putStrLn "Finished reading file"
+    putStrLn "---------------------"
     putStrLn . show $ rawAudio
 
-    -- Convert to bytestring for further processing
-    let bs = BS.pack .  elems .  sampleData $ rawAudio
-    putStrLn . show $ BS.length bs
-    case A.parseOnly (A.skipWhile (== 128) *> parseGlossika) bs of
-        Right s -> do 
-            let asAudio = fmap (toAudio . toSampleData . toList) s
-            zipWithM_ exportFile (fmap getFilename [1..]) asAudio
-            where
-                toList = BS.unpack
-                toSampleData = \x -> array (0, (length x) - 1) (zip [0..] x)
-                toAudio = \x -> Audio (sampleRate rawAudio) (channelNumber rawAudio) x
-        Left s -> putStrLn . show $ s
+    -- Convert to ByteString for Attoparsec parsing
+    let rawByteString = BS.pack .  elems .  sampleData $ rawAudio
+
+    -- Fast-forward parser to the good parts
+    let parseSplitOnPause' = A.skipWhile isSilence *> parseSplitOnPause
+
+    -- Run the Parser
+    let parseResults = A.parseOnly parseSplitOnPause' rawByteString
+
+    -- Create a function to rebuild the raw parsed results
+    let rebuild = fmap $ toAudio (sampleRate rawAudio) (channelNumber rawAudio)
+
+    -- Check the results
+    case parseResults of
+        Right results -> do
+            putStrLn "Parser success! Saving results..."
+            saveResults $ rebuild results
+            putStrLn "Done"
+
+        Left errorMsg -> do
+            putStrLn "Parser returned an error..."
+            putStrLn . show $ errorMsg
