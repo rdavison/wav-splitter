7560eb8c20b2725cfe98889fcc729dacfeed168b
Richard Davison <richivinsky@gmail.com>
2015-08-05 01:51:20 -0400

    Finally can split and recombine!  Needs major cleanup work though...

--- src/Main.hs
+++ src/Main.hs
@@ -5,8 +5,9 @@ module Main where
 import Codec.Wav (importFile, exportFile)
 import qualified Codec.ByteString.Parser as CBP
 import Control.Applicative (many)
-import Control.Monad (zipWithM_)
-import Data.Array.IArray (elems)
+import Control.Monad (zipWithM_, liftM)
+import Data.Array.IArray (elems, listArray, bounds)
+import Data.Array.Unboxed (UArray)
 import Data.Attoparsec.Binary (anyWord16le)
 import Data.Attoparsec.ByteString.Lazy (Parser, Result(..), parse)
 import qualified Data.Attoparsec.ByteString as DAB
@@ -15,18 +16,20 @@ import Data.Audio (Audio(..), SampleData(..), sampleData, parseSampleData)
 import Data.ByteString.Lazy (ByteString(..))
 import Data.ByteString.Builder (int16LE, toLazyByteString)
 import Data.Either (either)
-import Data.Foldable (foldMap)
+import Data.Foldable (foldMap, toList)
 import Data.Int (Int16)
---import Prelude hiding (takeWhile, take)
+import Data.List (last, intercalate)
+import Data.List.Split (splitOn)
+import qualified Data.Sequence as Seq
 import System.Environment (getArgs)
 import System.FilePath (splitExtension, joinPath, splitDirectories)
 
 
+--TODO: Cleanup all this crap
+
+
 toByteString :: Audio Int16 -> ByteString
 toByteString = toLazyByteString . foldMap int16LE . elems . sampleData
-
-toByteString' :: Audio Int16 -> ByteString
-toByteString' = toLazyByteString . foldMap int16LE . elems . sampleData
 
 
 parseSilenceByte :: Parser Int16
@@ -113,7 +116,7 @@ extractChunks xs = mapM (uncurry extractChunk) xs
 sliceAudio :: Audio Int16 -> [(Int, Int)] -> [Audio Int16]
 sliceAudio audio indices = either (const []) audioSlices parseResults
     where
-        parseResults = CBP.runParser (extractChunks indices) (toByteString' audio)
+        parseResults = CBP.runParser (extractChunks indices) (toByteString audio)
         audioSlices = fmap (\sd -> Audio (sampleRate audio) (channelNumber audio) sd)
 
 
@@ -126,21 +129,53 @@ process fp audio = do
         Done rem indexPairs -> do
             putStrLn "Success..."
             putStrLn . show $ indexPairs
-            let slices = sliceAudio audio indexPairs -- (take 5 indexPairs) -- [indexPairs !! 0, indexPairs !! 1]
+            let slices = sliceAudio audio indexPairs
             let ofps = zipWith getOutputFilePath (repeat fp) [1..]
             zipWithM_ exportFile ofps slices
-            --mapM_ (putStrLn.show.elems.sampleData) slices
 
 
-runApp :: FilePath -> IO ()
-runApp fp = putStrLn ("Reading File: " ++ fp) >> importFile fp >>= either skip (process fp)
+runSplit :: [FilePath] -> IO ()
+runSplit fps = mapM_ go fps
     where
+        go :: FilePath -> IO ()
+        go fp = putStrLn ("Reading File: " ++ fp) >> importFile fp >>= either skip (process fp)
         skip :: String -> IO ()
         skip msg = putStrLn $ "Error: " ++ msg ++ "... skipping file..."
+
+
+-- TODO: Fix this shit
+getCombinedOutputFilePath :: [FilePath] -> FilePath
+getCombinedOutputFilePath fps' = joinPath ["combined", combinedFilename]
+    where
+        fps = tail fps'
+        getFilename = fst . splitExtension . last . splitDirectories
+        getExtension = snd . splitExtension . last . splitDirectories
+        getLanguage = drop 2 . (!! 1) . splitOn "-"
+        langs = "EN-" ++ (intercalate "-" $ fmap (getLanguage . getFilename) fps)
+        combinedFilename = intercalate "-" $ toList $ Seq.update 1 langs $ Seq.fromList $ splitOn "-" . last . splitDirectories $ head fps
+
+
+combineSampleData :: [SampleData Int16] -> SampleData Int16
+combineSampleData sds =
+    let pauseSize = 44100
+        newSize = (foldl (\acc x -> acc + ((snd x) + 1)) 0 [bounds x | x <- sds]) + ((length sds) - 1) * pauseSize
+        newData = intercalate (take pauseSize (repeat (0 :: Int16))) [elems x | x <- sds]
+    in listArray (0, newSize-1) newData :: UArray Int Int16
+
+
+runCombine :: [FilePath] -> IO ()
+runCombine fps = do
+    audioList <- sequence $ fps >>= return . (fmap $ either undefined id) . importFile
+    let a = head audioList
+    let fp = head fps
+    let combinedAudio = Audio (sampleRate a) (channelNumber a) (combineSampleData $ audioList >>= return . sampleData)
+    exportFile (getCombinedOutputFilePath fps) combinedAudio
+    return ()
 
 
 main :: IO ()
 main = do
     args <- getArgs
-    mapM_ runApp args
+    let runApp = let app' = args !! 0 in if app' == "combine" then runCombine else runSplit
+    runApp $ tail args
     return ()
